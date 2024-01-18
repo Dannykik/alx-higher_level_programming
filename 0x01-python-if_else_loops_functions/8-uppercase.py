9ac875729e40299b5a312a3d1895c56bdf82c48e
Danny KIKWAYA <dannykikkikwaya1@gmail.com>
2024-01-18 14:22:38 -0800

    Task 9

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def uppercase(str):
+    res = ""
+    for i in str:
+        if ord("a") <= ord(i) <= ord("z"):
+             res += chr(ord(i) - 32)
+        else:
+            res += i
+    print(res)
