4dec527b4838f6c441c03cca56f6bc9ad87e6560
Danny KIKWAYA <dannykikkikwaya1@gmail.com>
2024-01-21 15:40:19 -0800

    First task

--- add_0.py
+++ add_0.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
+
 def add(a, b):
-    """My addition function
+    return a + b
 
-    Args:
-        a: first integer
-        b: second integer
-
-    Returns:
-        The return value. a + b
-    """
-    return (a + b)
+if __name__ == "__main__":
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
