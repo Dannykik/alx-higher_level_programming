8771339ddd56552ecec09e3a9f27071822dd4d38
Danny KIKWAYA <dannykikkikwaya1@gmail.com>
2024-01-22 16:06:32 -0800

    Task 5

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    new_list = list(my_string)
-    for i in new_list:
-        if ord(i) == 99:
-            new_list.remove(i)
-    result = ''.join(new_list)
+    char_list = list(my_string)
+    char_list = [char for char in char_list if char.lower() != 'c']
+    result = ''.join(char_list)
     return result
